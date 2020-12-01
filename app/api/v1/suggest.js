528fe70ec8ea4c853a5f5e515ffe1f980e0f65bb
couy <604696527@qq.com>
2020-12-01 15:16:54 +0800

    添加评论查询接口

--- app/api/v1/suggest.js
+++ app/api/v1/suggest.js
@@ -39,6 +39,25 @@ router.post('/get', async (ctx) => {
           [Op.like]: '%' + body.name || '' + '%'
         },
       },
+      order:[['created_at', 'DESC']],
+      limit: parseInt(body.size || 10),
+      offset: parseInt((body.size * (body.page-1)) || 0)
+    })
+    throw new global.errs.Success('操作成功', r)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+})
+
+router.post('/getReviewed', async (ctx) => {
+  try {
+    var body = ctx.request.body
+    const r = await Suggest.findAndCountAll({
+      where: {
+        standby2: '1',
+      },
+      order:[['created_at', 'DESC']],
       limit: parseInt(body.size || 10),
       offset: parseInt((body.size * (body.page-1)) || 0)
     })
@@ -54,6 +73,7 @@ router.post('/reply', async (ctx) => {
     var body = ctx.request.body
     const r = await Suggest.findByPk(body.id);
     r.standby1 = body.reply
+    r.standby2 = 1
     await r.save()
     throw new global.errs.Success('操作成功')
   } catch (error) {
